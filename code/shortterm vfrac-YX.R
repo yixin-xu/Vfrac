484328f01a755c765b35f0f0335932e0f162f894
Bogdasayen <howard.thom@bristol.ac.uk>
2021-04-03 17:49:00 +0100

    Merged cost and qaly loop

--- code/shortterm vfrac-YX.R
+++ code/shortterm vfrac-YX.R
@@ -6,23 +6,18 @@ patient_infor_raw = read.csv('results/qalys and costs.csv')
 patient_infor = patient_infor_raw%>% drop_na(36)
 
 
-
+# Calculate costs and QALYs depending on whether they would be referred for x-ray by vfrac
 for( i in 1:nrow(patient_infor)){
   if(patient_infor$xvfracscreen[i] == 0){
-    patient_infor$qalys[i] = patient_infor$beq5d_score[i]*0.25}
+    patient_infor$qalys[i] = patient_infor$beq5d_score[i]*0.25
+    patient_infor$costs[i] = patient_infor$bcosts[i]
+    }
   if(patient_infor$xvfracscreen[i] == 1){
-    patient_infor$qalys[i] = patient_infor$feq5d_score[i]*0.25}
+    patient_infor$qalys[i] = patient_infor$feq5d_score[i]*0.25
+    patient_infor$costs[i] = patient_infor$fcosts[i]
+    }
 
 }
-
-for( i in 1:nrow(patient_infor)){
-  if(patient_infor$xvfracscreen[i] == 0){
-    patient_infor$costs[i] = patient_infor$bcosts[i]}
-  if(patient_infor$xvfracscreen[i] == 1){
-    patient_infor$costs[i] = patient_infor$fcosts[i]}
-  
-}
-
 
 shortterm_vfrac = data.frame(patient_infor$qalys,patient_infor$costs)
 
